6175a6f4ba44a838b35772ce4bd2a1bbf558a362
Kate <avdoshka1@yandex.ru>
2023-11-27 20:08:23 +0400

    commit

--- src/BonusMilesService.java
+++ src/BonusMilesService.java
@@ -1,5 +1,5 @@
 public class BonusMilesService {
-    public int calculate(int ticketPrice){
+    public int calculate(int ticketPrice) {
         int bonusMiles;
         bonusMiles = ticketPrice / 20;
         return bonusMiles;
